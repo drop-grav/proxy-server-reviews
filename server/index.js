03827738b0ffb87dab5691af9da4e9267490b929
wei chen <chenwei1356@gmail.com>
2019-09-26 20:47:27 -0700

    finish proxy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
+const newRelic = require('newrelic');
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/../src'));
 app.use('/:id', express.static(__dirname + '/../src'));
-const PORT = 3001;
+const PORT = 80;
 app.listen(PORT, () => console.log('Proxy set'));
+
+app.get('/api/listing/:listingID/photos', (req, res) => {
+  const { listingID } = req.params;
+  res.redirect(`http://ec2-54-183-195-148.us-west-1.compute.amazonaws.com:3100/api/listing/${listingID}`);
+ });
