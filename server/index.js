ce237f14c6ad230bae96b1729e262f01d18deb38
wei chen <chenwei1356@gmail.com>
2019-09-26 20:58:41 -0700

    finish proxy

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/../src'));
 app.use('/:id', express.static(__dirname + '/../src'));
+app.use('/listing/:id', express.static(__dirname + '/../src'));
 const PORT = 3000;
 app.listen(PORT, () => console.log('Proxy set'));
 
