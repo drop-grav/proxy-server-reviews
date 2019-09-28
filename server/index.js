e77cd2ee247e5dc2c2e624a160e5de7d1c3b0c30
wei chen <chenwei1356@gmail.com>
2019-09-27 17:39:20 -0700

    update proxy

--- server/index.js
+++ server/index.js
@@ -11,3 +11,18 @@ app.get('/api/listing/:listingID/photos', (req, res) => {
   const { listingID } = req.params;
   res.redirect(`http://ec2-54-183-195-148.us-west-1.compute.amazonaws.com:3100/api/listing/${listingID}`);
  });
+
+app.post('/api/rooms/:roomId/reviews', (req, res) => {
+  const { roomId } = req.params;
+  res.redirect(`http://13.57.224.21/api/rooms/${roomId}/reviews`);
+});
+
+app.put('/api/rooms/:roomId/reviews/:reviewId', (req, res) => {
+  const { roomId, reviewId } = req.params;
+  res.redirect(`http://13.57.224.21/api/rooms/${roomId}/reviews/${reviewId}`);
+});
+
+app.delete('/api/rooms/:roomId/reviews/:reviewId', (req, res) => {
+  const { roomId, reviewId } = req.params;
+  res.redirect(`http://13.57.224.21/api/rooms/${roomId}/reviews/${reviewId}`);
+});
